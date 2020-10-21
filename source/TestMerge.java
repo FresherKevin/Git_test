e02e1a2c436cb831113246309e68f142e65a378f
xck <chaokai_xu@163.com>
2020-10-21 17:18:28 +0800

    test merge

---
+++ source/TestMerge.java
@@ -0,0 +1,14 @@
+/**
+ * <p> 名称：TestMerge </p>
+ * <p> 描述：测试合并代码冲突 </p>
+ * <p> 版本：v1.0.0 </p>
+ * <p> 日期：2020-10-21 17:16 </p>
+ *
+ * <p> @author：xck </p>
+ */
+public class TestMerge {
+
+	public static void main(String[] args) {
+
+	}
+}
